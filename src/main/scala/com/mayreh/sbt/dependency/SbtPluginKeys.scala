421da9447d99f6740286623a9eb63a85429b0014
ocadaruma <ocadaruma@gmail.com>
2017-10-18 22:31:49 +0900

    Adopt to sbt 1.0

--- src/main/scala/com/mayreh/sbt/dependency/SbtPluginKeys.scala
+++ src/main/scala/com/mayreh/sbt/dependency/SbtPluginKeys.scala
@@ -3,16 +3,11 @@ package com.mayreh.sbt.dependency
 import sbt._
 
 trait SbtPluginKeys {
-  val reverseDependencySeparator = SettingKey[String]("reverse-dependency-separator",
-    "Specify separator string for printing projects.")
-  val printBaseDirectory = SettingKey[Boolean]("print-base-directory",
-    "Print base directory instead of project ID.")
-  val printAbsolutePath = SettingKey[Boolean]("print-absolute-path",
-    "Print absolute path instead of related path. This option only affects when printBaseDirectory is true.")
-  val printReverseDependency = TaskKey[Unit]("print-reverse-dependency",
-    "Print which projects are depend on current project.")
-  val reverseDependency = TaskKey[Seq[ResolvedProject]]("reverse-dependency",
-    "Return which projects are depend on current project.")
+  val reverseDependencySeparator = settingKey[String]("Specify separator string for printing projects.")
+  val printBaseDirectory = settingKey[Boolean]("Print base directory instead of project ID.")
+  val printAbsolutePath = settingKey[Boolean]("Print absolute path instead of related path. This option only affects when printBaseDirectory is true.")
+  val printReverseDependency = taskKey[Unit]("Print which projects are depend on current project.")
+  val reverseDependency = taskKey[Seq[ResolvedProject]]("Return which projects are depend on current project.")
 }
 
 object AttributeKeys {
